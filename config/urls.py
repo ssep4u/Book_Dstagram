06947c4f8ff87b13e0ccc70ee19f9099516dc273
SSep <ssep4u@gmail.com>
2020-02-16 21:36:07 +0900

    config/urls accounts/urls login, logout

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('photo.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
