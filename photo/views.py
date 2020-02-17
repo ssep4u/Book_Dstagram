bf634c4fab55464e902ef21ea4c099ba291acba7
SSep <ssep4u@gmail.com>
2020-02-18 00:00:25 +0900

    require login photo/views @login_required, LoginRequiredMixin

--- photo/views.py
+++ photo/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView
 
 from photo.models import Photo
 
 
+@login_required
 def photo_list(request):
   photos = Photo.objects.all()
   return render(request, 'photo/list.html', {'photos': photos})
 
 
-class PhotoUploadView(CreateView):
+class PhotoUploadView(LoginRequiredMixin, CreateView):
   model = Photo
   fields = ['photo', 'text']
   template_name = 'photo/upload.html'
@@ -23,13 +26,13 @@ class PhotoUploadView(CreateView):
       return self.render_to_response({'form': form})
 
 
-class PhotoDeleteView(DeleteView):
+class PhotoDeleteView(LoginRequiredMixin, DeleteView):
   model = Photo
   success_url = '/'
   template_name = 'photo/delete.html'
 
 
-class PhotoUpdateView(UpdateView):
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
   model = Photo
   fields = ['photo', 'text']
   template_name = 'photo/update.html'
